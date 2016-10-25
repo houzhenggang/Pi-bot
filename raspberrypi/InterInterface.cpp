080c3c9202e259d0ab5b68abe5d9024320f06346
Kieran Wyse <kieranwyse@gmail.com>
2016-10-25 17:30:04 +0100

    changes to function binding

--- raspberrypi/InterInterface.cpp
+++ raspberrypi/InterInterface.cpp
@@ -23,8 +23,8 @@ Don't worry if this seems ludicrously hard -- your question intersects several o
 wireingPIISR is the interupt method in wiringpi Library. It will call the specified function each time the pin goes high ( rising) or low (falling)
 
   */
-  auto hl = std::bind(&InterInterface::trigLowHigh,this);
-  auto lh = std::bind(&InterInterface::trigHighLow,this);
+  auto hl = std::bind(InterInterface::trigLowHigh,this);
+  auto lh = std::bind(InterInterface::trigHighLow,this);
   wiringPiISR (_pin, INT_EDGE_RISING,hl ) ;
   wiringPiISR (_pin, INT_EDGE_FALLING, lh) ;
 }
